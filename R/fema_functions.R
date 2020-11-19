841fc95b80df2837f2dd096034fa69a0a60d47a2
Shea-Fyffe <shea.fyffe@gmail.com>
2020-11-19 13:39:45 -0500

    Shea: deleted state data

--- R/fema_functions.R
+++ R/fema_functions.R
@@ -107,11 +107,3 @@ if (httr::http_error(.res)) {
   .res <- jsonlite::fromJSON(api_url)
 return(.res)
 }
-# Dont really need this anymore
-# fema_state_codes <- function() {
-# c("AL","AK","AS","AZ","AR","CA","CO","CT","DE","DC","FL","GA","GU","HI",
-#   "ID","IL","IN","IA","KS","KY","LA","ME","MD","MH","MA","MI","FM","MN",
-#   "MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","MP","OH","OK",
-#   "OR","PW","PA","PR","RI","SC","SD","TN","TX","UT","VT","VA","VI","WA",
-#   "WV","WI","WY")
-# }
