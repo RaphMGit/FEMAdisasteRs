db86f213dd5335603a12737c49d9448ebb80b841
Martin Chamberlin <martinchamberlin@Martins-Air.fios-router.home>
2020-12-01 18:26:00 -0500

    Dec 1 New function map_fema_data; MC pass data from get_fema_data. call GGplot and make stacked bar chart

---
+++ map_fema_data.R
@@ -0,0 +1,21 @@
+# map_fema_data
+# GMU team 2 MC
+# get_fema_data returns DisasterDeclarationsSummaries
+#
+# get_fema_data into a DataFrame for our test case.
+entity <- "DisasterDeclarationsSummaries"
+api_params <-  "filter=fyDeclared gt 1979"
+FEMAdf <- get_fema_data(entity, api_params)
+
+library("ggplot2")
+library(RColorBrewer)
+map_fema_data <- function(x) {
+    morecolors = c(brewer.pal(12,"Paired"),"#000000")
+    MapGG <- ggplot(FEMAdf, aes(state),fill=incidentType) + geom_bar(aes(fill=FEMAdf$incidentType), position=position_stack(), show.legend=TRUE) + 
+        scale_fill_manual(values = morecolors)
+    return(MapGG)
+    } 
+   
+# try it 
+map_fema_data(FEMAdf)
+# END OF FILE
