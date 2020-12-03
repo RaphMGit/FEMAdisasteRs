e38c7348a68a4f255d5a1c821e4b7f166df3b997
Shea-Fyffe <shea.fyffe@gmail.comgit config --global user.email shea.fyffe@gmail.comgit config --global user.email shea.fyffe@gmail.com>
2020-12-03 15:11:24 -0500

    Shea-Fyffe: styled code to be a bit nicer.

--- R/fema_functions.R
+++ R/fema_functions.R
@@ -1,6 +1,6 @@
 #' Get FEMA Open API Data
 #'
-#' @param entity Character. Name of entity to scrape. See function \code{\link{get_fema_entites}} 
+#' @param entity Character. Name of entity to scrape. See function \code{\link{get_fema_entites}}
 #'  documentation for details.
 #' @param api_params Named list. Extra arguments to pass to api. See \url{https://www.fema.gov/about/openfema/api}.
 #' Parameters are validated using the \code{\link{check_api_params}} function.
@@ -8,25 +8,25 @@
 #'
 #' @return A dataset taken from \url{https://www.fema.gov/api/open}
 #'
-#' @seealso \code{\link{get_fema_entites}} \code{\link{get_fema_data_all}} 
-#' @examples 
-#' 
+#' @seealso \code{\link{get_fema_entites}} \code{\link{get_fema_data_all}}
+#' @examples
+#'
 #' ## Get first 1000 records from FEMA Web Disasters
 #' get_fema_data("FEMA Web Disasters", api_params = list(skip = 1000))
-#' 
+#'
 #' ## Get first 1000 records from FEMA Web Disasters after skipping first 1000 records
 #' get_fema_data("FEMA Web Disasters", api_params = list(skip = 1000))
-#' 
+#'
 #' ## Get first 1000 records ordered by state from Disaster Declarations Summaries after skipping first 1000 records
-#' get_fema_data("Disaster Declarations Summaries", 
-#' api_params = list(orderby = state, skip = 1000))
-#' 
+#' get_fema_data("Disaster Declarations Summaries",
+#'   api_params = list(orderby = state, skip = 1000)
+#' )
 #' @import httr
 #' @export
 get_fema_data <- function(entity, api_params = list(), base_url = "https://www.fema.gov/api/open") {
   entity_info <- get_fema_entities(entity)
-  api_url <- paste0(base_url,"/", tolower(entity_info$OpenFEMAAPI), "/", entity_info$Entity_API_string)
-  if(length(api_params) != 0) {
+  api_url <- paste0(base_url, "/", tolower(entity_info$OpenFEMAAPI), "/", entity_info$Entity_API_string)
+  if (length(api_params) != 0) {
     api_params <- check_api_params(api_params)
     api_url <- paste0(api_url, api_params)
   }
@@ -59,18 +59,20 @@ get_fema_entities <- function(entity, entity_url = "https://www.fema.gov/about/o
   entity_table <- rvest::html_table(entity_table)
   entity_table <- do.call(rbind, entity_table)
   entity_table[entity_table == "N/A"] <- NA
-  entity_table <- entity_table[!is.na(entity_table[,2]), -3]
+  entity_table <- entity_table[!is.na(entity_table[, 2]), -3]
   names(entity_table) <- c("Entity", gsub(" ", "", names(entity_table)[-1]))
   entity_table$Entity_API_string <- .format_entity(entity_table$Entity)
-  if(verbose) {
+  if (verbose) {
     return(entity_table)
   } else {
     found <- match(.format_entity(entity), entity_table$Entity_API_string)
-    if(is.na(found)) {
-      stop(paste0("Entity not found. Here are availible entities:", "\n\n",
-                  paste0(entity_table$Entity_API_string, collapse = "\n")))
+    if (is.na(found)) {
+      stop(paste0(
+        "Entity not found. Here are availible entities:", "\n\n",
+        paste0(entity_table$Entity_API_string, collapse = "\n")
+      ))
     } else {
-      return(entity_table[found,])
+      return(entity_table[found, ])
     }
   }
 }
@@ -78,22 +80,28 @@ get_fema_entities <- function(entity, entity_url = "https://www.fema.gov/about/o
 #'
 #' @param .api_params Named list taken from \code{api_params} argument in
 #' \code{\link{get_fema_data}} \code{\link{get_fema_data_all}}
-#' @export 
+#' @export
 check_api_params <- function(.api_params) {
   if (!inherits(.api_params, "list")) {
     stop("api parameters must be a named list")
   }
-  api_cmds <- c("callback", "filename", "filter", 
-                "format","inlinecount", "metadata",
-                "orderby", "select", "skip", "top")
-  
+  api_cmds <- c(
+    "callback", "filename", "filter",
+    "format", "inlinecount", "metadata",
+    "orderby", "select", "skip", "top"
+  )
+
   if (!all(names(.api_params) %in% api_cmds)) {
-     stop(paste0("Invalid api parameter. Availible parameters are: ", 
-                paste0(api_cmds, collapse = ";")))
+    stop(paste0(
+      "Invalid api parameter. Availible parameters are: ",
+      paste0(api_cmds, collapse = ";")
+    ))
   }
   if ("filter" %in% names(.api_params)) {
-    .filter_cmd <- c("contains", "geo\\.intersects", "startswith", "endswith", "substringof",
-                     "eq", "ne", "gt", "ge", "lt", "le", "and", "or", "not")
+    .filter_cmd <- c(
+      "contains", "geo\\.intersects", "startswith", "endswith", "substringof",
+      "eq", "ne", "gt", "ge", "lt", "le", "and", "or", "not"
+    )
     .filter_test <- !grepl(paste0(sprintf("\\b%s\\b", .filter_cmd), collapse = "|"), .api_params[["filter"]])
     if (.filter_test || length(.filter_test) != 1) {
       stop("api filter syntax incorrect or not a single string, 
@@ -101,7 +109,7 @@ check_api_params <- function(.api_params) {
     }
   }
   .api_params <- lapply(.api_params, function(x) {
-   x <-  URLencode(as.character(x), reserved = TRUE)
+    x <- URLencode(as.character(x), reserved = TRUE)
   })
   .api_params <- paste0("$", names(.api_params), "=", .api_params)
   .api_params <- paste0("?", paste0(.api_params, collapse = "&"))
@@ -110,7 +118,7 @@ check_api_params <- function(.api_params) {
 #' Get FEMA Open API All Data
 #'
 #' @inheritParams get_fema_data
-#' @param max_limit To load and stop at a certain number. 
+#' @param max_limit To load and stop at a certain number.
 #' @param wait Number of seconds to process the data.
 #'
 #' @return
@@ -126,7 +134,7 @@ get_fema_data_all <- function(entity, api_params = list(),
     stop("Not enough records to download try get_fema_data")
   }
   skips <- seq(0, first_page$metadata$count, 1000)[seq(0, first_page$metadata$count, 1000) <= max_limit]
-  api_urls <- paste0(all_data_url,"&$skip=", skips)[-1]
+  api_urls <- paste0(all_data_url, "&$skip=", skips)[-1]
   res <- lapply(api_urls, query_fema, .wait = wait)
   res <- c(first_page, res)
   return(res)
@@ -144,19 +152,19 @@ get_fema_data_all <- function(entity, api_params = list(),
 query_fema <- function(api_url, .wait = 1) {
   Sys.sleep(.wait)
   .res <- httr::GET(api_url)
-# this check to see if the website wasn't found (could be simpler)
-if (httr::http_error(.res)) {
-  if(.res$status == 404) {
-    .err <- "Website not found"
-  } else if (.res$status == 400) {
-    .err <- "Params not availible for entity"
-  } else {
-    .err <- paste0("Error please check the following link
+  # this check to see if the website wasn't found (could be simpler)
+  if (httr::http_error(.res)) {
+    if (.res$status == 404) {
+      .err <- "Website not found"
+    } else if (.res$status == 400) {
+      .err <- "Params not availible for entity"
+    } else {
+      .err <- paste0("Error please check the following link
                      in your browser: ", .res$url)
+    }
+    stop(.err)
   }
-  stop(.err)
-}
-  .res <- httr::content(.res, as ="text")
+  .res <- httr::content(.res, as = "text")
   .res <- jsonlite::fromJSON(api_url)
-return(.res)
+  return(.res)
 }
--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,49 +1,55 @@
 #' Map FEMA data to Stacked Bar
 #' @author Martin Chamberlin
 #'
-#' @param df Required Data.frame created by \code{\link{get_fema_data}} or 
+#' @param df Required Data.frame created by \code{\link{get_fema_data}} or
 #' \code{\link{get_fema_data_all}}
 #' @param x_var Optional Character string. Column name in \code{df} to be placed on x axis. state by default.
-#' @param by  Optional Character string. Column name in \code{df} to be used as fill. 
+#' @param by  Optional Character string. Column name in \code{df} to be used as fill.
 #' incidentType by default.
 #'
 #' @return A stacked \code{\link{[ggplot2](geom_bar)}}
 #'
 #' @examples
-#' 
+#'
 #' ## Assign entity to be used to get data
 #' entity <- "DisasterDeclarationsSummaries"
-#' 
+#'
 #' ## get data when fiscal year is greater than 1979
 #' FEMAdf <- get_fema_data(entity, api_params = list(filter = 'fyDeclared gt "1979"'))
 #'
 #' ## plot data
 #' map_fema_data(FEMAdf[[2]])
-#' 
-#' @seealso \code{\link{get_fema_data}} \code{\link{get_fema_data_all}} for getting fema data 
-#' 
+#' @seealso \code{\link{get_fema_data}} \code{\link{get_fema_data_all}} for getting fema data
+#'
 #' @import ggplot2 RColorBrewer
 #' @export
 map_fema_data <- function(df, x_var = "state", by = "incidentType") {
-  if(!inherits(df, "data.frame")) {
+  if (!inherits(df, "data.frame")) {
     stop("df must be a dataframe")
   }
-  if (!all(c(x_var,by) %in% names(df))) {
+  if (!all(c(x_var, by) %in% names(df))) {
     stop("x_var or by column not found in dataset df")
   }
   .n_by <- length(unique(df[, by]))
-  if(.n_by > 12) {
-    morecolors <- c(RColorBrewer::brewer.pal(12, "Paired"),
-                    RColorBrewer::brewer.pal(.n_by - 12, "Set2"))
+  if (.n_by > 12) {
+    morecolors <- c(
+      RColorBrewer::brewer.pal(12, "Paired"),
+      RColorBrewer::brewer.pal(.n_by - 12, "Set2")
+    )
   } else {
     morecolors <- RColorBrewer::brewer.pal(.n_by, "Paired")
   }
 
-  MapGG <- ggplot2::ggplot(data = df, ggplot2::aes_string(x = x_var)) + 
-    ggplot2::geom_bar(ggplot2::aes_string(fill = by), position = 
-                        ggplot2::position_stack(), show.legend=TRUE) + 
-    ggplot2::scale_fill_manual(values = morecolors) + ggplot2::theme_minimal() +
-    ggplot2::theme(legend.position = "top", legend.title = ggplot2::element_blank(),
-                   legend.text = ggplot2::element_text(size = 8))
+  MapGG <- ggplot2::ggplot(data = df, ggplot2::aes_string(x = x_var)) +
+    ggplot2::geom_bar(ggplot2::aes_string(fill = by),
+      position =
+        ggplot2::position_stack(), show.legend = TRUE
+    ) +
+    ggplot2::scale_fill_manual(values = morecolors) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "top", legend.title = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(size = 8)
+    )
   return(MapGG)
-} 
+}
