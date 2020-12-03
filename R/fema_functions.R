598b444b0ec0a7897d74bb0dda6c9681aa29c9e9
Shea-Fyffe <shea.fyffe@gmail.comgit config --global user.email shea.fyffe@gmail.comgit config --global user.email shea.fyffe@gmail.com>
2020-12-03 15:09:19 -0500

    shea-fyffe: reworked format_entity() needs testing.

--- R/fema_functions.R
+++ R/fema_functions.R
@@ -1,15 +1,29 @@
-#' @title Get FEMA Open API Data
+#' Get FEMA Open API Data
 #'
-#' @param entity Character. Name of entity to scrape. See function `get_fema_entites()` for details.
+#' @param entity Character. Name of entity to scrape. See function \code{\link{get_fema_entites}} 
+#'  documentation for details.
+#' @param api_params Named list. Extra arguments to pass to api. See \url{https://www.fema.gov/about/openfema/api}.
+#' Parameters are validated using the \code{\link{check_api_params}} function.
 #' @param base_url Character. "https://www.fema.gov/api/open" (default),  website Url of FEMAs API.
-#' @param api_params named list. extra arguments to pass to api
 #'
-#' @return A dataset taken from \link{}
+#' @return A dataset taken from \url{https://www.fema.gov/api/open}
+#'
+#' @seealso \code{\link{get_fema_entites}} \code{\link{get_fema_data_all}} 
+#' @examples 
+#' 
+#' ## Get first 1000 records from FEMA Web Disasters
+#' get_fema_data("FEMA Web Disasters", api_params = list(skip = 1000))
+#' 
+#' ## Get first 1000 records from FEMA Web Disasters after skipping first 1000 records
+#' get_fema_data("FEMA Web Disasters", api_params = list(skip = 1000))
+#' 
+#' ## Get first 1000 records ordered by state from Disaster Declarations Summaries after skipping first 1000 records
+#' get_fema_data("Disaster Declarations Summaries", 
+#' api_params = list(orderby = state, skip = 1000))
+#' 
 #' @import httr
 #' @export
-#'
-#' @see also \link{https://www.fema.gov/about/openfema/api}
-get_fema_data <- function(entity, base_url = "https://www.fema.gov/api/open", api_params = list()) {
+get_fema_data <- function(entity, api_params = list(), base_url = "https://www.fema.gov/api/open") {
   entity_info <- get_fema_entities(entity)
   api_url <- paste0(base_url,"/", tolower(entity_info$OpenFEMAAPI), "/", entity_info$Entity_API_string)
   if(length(api_params) != 0) {
@@ -32,7 +46,11 @@ get_fema_data <- function(entity, base_url = "https://www.fema.gov/api/open", ap
 #' @examples
 get_fema_entities <- function(entity, entity_url = "https://www.fema.gov/about/openfema/data-sets", verbose = FALSE) {
   .format_entity <- function(x) {
-    if (all(grepl("\\w+\\s+\\w+", x))) {
+    if (all(grepl("^[a-z]+$|^[A-Z]+$", x))) {
+      stop("enitity must contain spaces or a variety of lower and uppercase letters")
+    } else {
+      x <- strsplit(x, split = "(\\s+)|(?<=.)(?=[A-Z][a-z])", perl = T)
+      x <- sapply(x, paste0, collapse = " ")
       x <- gsub("[^A-z]+", "", tools::toTitleCase(tolower(x)))
     }
     return(x)
@@ -56,30 +74,42 @@ get_fema_entities <- function(entity, entity_url = "https://www.fema.gov/about/o
     }
   }
 }
-#' this check to make sure that your extra api commands are valid
+#' Validate API parameters
 #'
-#' @param params 
-check_api_params <- function(params) {
+#' @param .api_params Named list taken from \code{api_params} argument in
+#' \code{\link{get_fema_data}} \code{\link{get_fema_data_all}}
+#' @export 
+check_api_params <- function(.api_params) {
+  if (!inherits(.api_params, "list")) {
+    stop("api parameters must be a named list")
+  }
   api_cmds <- c("callback", "filename", "filter", 
                 "format","inlinecount", "metadata",
                 "orderby", "select", "skip", "top")
-  if (!all(names(params) %in% api_cmds)) {
+  
+  if (!all(names(.api_params) %in% api_cmds)) {
      stop(paste0("Invalid api parameter. Availible parameters are: ", 
                 paste0(api_cmds, collapse = ";")))
   }
-  params <- lapply(params, function(x) {
+  if ("filter" %in% names(.api_params)) {
+    .filter_cmd <- c("contains", "geo\\.intersects", "startswith", "endswith", "substringof",
+                     "eq", "ne", "gt", "ge", "lt", "le", "and", "or", "not")
+    .filter_test <- !grepl(paste0(sprintf("\\b%s\\b", .filter_cmd), collapse = "|"), .api_params[["filter"]])
+    if (.filter_test || length(.filter_test) != 1) {
+      stop("api filter syntax incorrect or not a single string, 
+           please see api documentation for details")
+    }
+  }
+  .api_params <- lapply(.api_params, function(x) {
    x <-  URLencode(as.character(x), reserved = TRUE)
   })
-  params <- paste0("$", names(params), "=", params)
-  params <- paste0("?", paste0(params, collapse = "&"))
-  return(params)
+  .api_params <- paste0("$", names(.api_params), "=", .api_params)
+  .api_params <- paste0("?", paste0(.api_params, collapse = "&"))
+  return(.api_params)
 }
-
-#' Title
+#' Get FEMA Open API All Data
 #'
-#' @param entity Character. Name of entity to scrape.
-#' @param base_url Character. "https://www.fema.gov/api/open" Website URL or FEMA's API
-#' @param api_params A named list. Extra arguments to pass to API
+#' @inheritParams get_fema_data
 #' @param max_limit To load and stop at a certain number. 
 #' @param wait Number of seconds to process the data.
 #'
@@ -87,12 +117,12 @@ check_api_params <- function(params) {
 #' @export
 #'
 #' @examples
-get_fema_data_all <- function(entity, base_url = "https://www.fema.gov/api/open", api_params = list(),
-                              max_limit = 5000, wait = 1) {
+get_fema_data_all <- function(entity, api_params = list(),
+                              max_limit = 5000, wait = 1, base_url = "https://www.fema.gov/api/open") {
   api_params[["inlinecount"]] <- "allpages"
   first_page <- get_fema_data(entity, base_url, api_params)
   all_data_url <- paste0(gsub("\\/api\\/open", "", base_url), first_page$metadata$url)
-  if(first_page$metadata$count < 1000L) {
+  if (first_page$metadata$count < 1000L) {
     stop("Not enough records to download try get_fema_data")
   }
   skips <- seq(0, first_page$metadata$count, 1000)[seq(0, first_page$metadata$count, 1000) <= max_limit]
